95e92e1cf85720d404d78c487b17d936f3e6ed8d
feliwir <stephan.vedder@gmail.com>
2015-11-18 10:54:22 +0100

    Export Script functions when compiling with msvc, so they can be found by ffi

--- src/Script/Functions.cpp
+++ src/Script/Functions.cpp
@@ -3,14 +3,23 @@
 
 using namespace hpse;
 
-extern "C" void LoadGUI(const char * name)
-{
-	auto& gui  = Core::GetGUI();
-	gui->LoadFile(name);
-}
+#ifdef _MSC_VER 
+#define SCRIPT_FUNC __declspec(dllexport)
+#elif
+#define SCRIPT_FUNC
+#endif
 
-extern "C" void PrintRenderer()
+extern "C"
 {
-	auto renderer = Core::GetGraphics()->GetRenderer();
-	renderer->PrintInfo();
+	SCRIPT_FUNC void LoadGUI(const char * name)
+	{
+		auto& gui = Core::GetGUI();
+		gui->LoadFile(name);
+	}
+
+	SCRIPT_FUNC void PrintRenderer()
+	{
+		auto renderer = Core::GetGraphics()->GetRenderer();
+		renderer->PrintInfo();
+	}
 }
