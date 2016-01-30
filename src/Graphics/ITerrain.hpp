8df817275d8fdaa05e9b03ac538013d3a508bf10
feliwir <stephan.vedder@gmail.com>
2016-01-30 14:19:06 +0100

    Added Terrain baseclass where the OpenGL irrelevant code goes

---
+++ src/Graphics/ITerrain.hpp
@@ -0,0 +1,35 @@
+#pragma once
+#include "IRenderable.hpp"
+#include "Quadtree.hpp"
+#include <vector>
+#include <glm/glm.hpp>
+#include <memory>
+namespace hpse
+{
+	class ITerrain : public IRenderable
+	{
+	public:
+		ITerrain(std::uint32_t width, std::uint32_t height);
+		void SetTerrainHeight(glm::vec3 &pos, float height, float radius);
+	protected:
+		void Generate();
+		void ComputeNormals(std::vector<glm::vec3> &normals);
+		void UpdateBufferData();
+
+		std::uint32_t m_width, m_height;
+		std::vector<float> m_heightmap;
+		std::vector<std::string> m_terrainMaterials;
+		std::vector<glm::vec3> m_materialmap;
+		std::shared_ptr<Quadtree> m_quadtree;
+
+		std::vector<glm::vec3> m_vertices;
+		std::vector<glm::vec2> m_uvs;
+		std::vector<glm::vec3> m_normals;
+		std::vector<std::uint32_t> m_faces;
+		std::vector<glm::vec3> m_materials;
+		bool heightmap_changed = true;
+		bool uvs_changed = true;
+		bool faces_changed = true;
+		bool materials_changed = true;
+	};
+}
