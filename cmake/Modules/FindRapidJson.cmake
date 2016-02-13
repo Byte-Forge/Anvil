7b0280be0213a9fa70295402b79073c9817d1403
feliwir <stephan.vedder@gmail.com>
2016-02-13 12:35:06 +0100

    Customized FindRapidJson to work with travis

--- cmake/Modules/FindRapidJson.cmake
+++ cmake/Modules/FindRapidJson.cmake
@@ -1,12 +1,22 @@
 # - Find rapidjson headers and lib.
 # This module defines RAPIDJSON_INCLUDE_DIR, the directory containing headers
 
-set(RAPIDJSON_SEARCH_HEADER_PATHS
-  ${CMAKE_SOURCE_DIR}/thirdparty/rapidjson/include/
-)
+# check environment variable
+set(_rapidjson_ENV_ROOT_DIR "$ENV{RAPIDJSON_ROOT_DIR}")
 
-find_path(RAPIDJSON_INCLUDE_DIR rapidjson/rapidjson.h HINTS
-  ${RAPIDJSON_SEARCH_HEADER_PATHS})
+if(NOT RAPIDJSON_ROOT_DIR AND _rapidjson_ENV_ROOT_DIR)
+    SET(RAPIDJSON_ROOT_DIR "${_rapidjson_ENV_ROOT_DIR}")
+endif(NOT RAPIDJSON_ROOT_DIR AND _rapidjson_ENV_ROOT_DIR)
+
+# put user specified location at beginning of search
+if(RAPIDJSON_ROOT_DIR)
+    set(RAPIDJSON_SEARCH_HEADER_PATHS "${RAPIDJSON_ROOT_DIR}"
+                                "${RAPIDJSON_ROOT_DIR}/include"
+                                 ${RAPIDJSON_SEARCH_HEADER_PATHS})
+endif(RAPIDJSON_ROOT_DIR)
+
+find_path(RAPIDJSON_INCLUDE_DIR "rapidjson/rapidjson.h"
+    PATHS ${RAPIDJSON_SEARCH_HEADER_PATHS})
 
 if (RAPIDJSON_INCLUDE_DIR)
   set(RAPIDJSON_FOUND TRUE)
