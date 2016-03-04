44b709ecb1a5d6e3f1e2f2f54fe013baf971e505
Tarcontar <michael-schnabel@gmx.net>
2016-03-04 16:32:52 +0100

    fixed build error on linux

--- src/Util/Platform.cpp
+++ src/Util/Platform.cpp
@@ -50,7 +50,7 @@ std::vector<std::string> IO::ListFiles(const std::string & dir, const std::strin
 	while ((dirp = readdir(dp)) != NULL)
 	{
 		std::string name = dirp->d_name;
-		std::string fileExt = StringHelper::split(name, '.').back();
+		std::string fileExt = split(name, '.').back();
 
 		if (fileExt == ext || ext.size() == 0)
 			files.push_back(name);
