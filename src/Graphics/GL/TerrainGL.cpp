65578d1d1eb60e42f4d60621e7c1cefde0ad5c0b
feliwir <stephan.vedder@gmail.com>
2016-04-04 23:35:37 +0200

    Use nullptr

--- src/Graphics/GL/TerrainGL.cpp
+++ src/Graphics/GL/TerrainGL.cpp
@@ -41,28 +41,28 @@ GL::Terrain::Terrain(std::uint32_t width, std::uint32_t height) : ITerrain(width
 	m_vbo->Update(static_cast<unsigned int>(m_vertices.size() * sizeof(glm::vec3)), m_vertices.data());
 
 	glEnableVertexAttribArray(0);
-	glVertexAttribPointer(0, 3, GL_FLOAT, GL_FALSE, 0, (void*)0);
+	glVertexAttribPointer(0, 3, GL_FLOAT, GL_FALSE, 0, nullptr);
 
 	m_uvbo = std::make_unique<GL::Buffer>(ARRAY_BUFFER);
 	m_uvbo->Bind();
 	m_uvbo->Update(static_cast<unsigned int>(m_uvs.size() * sizeof(glm::vec2)), m_uvs.data());
 	
 	glEnableVertexAttribArray(1);
-	glVertexAttribPointer(1, 2, GL_FLOAT, GL_FALSE, 0, (void*)0);
+	glVertexAttribPointer(1, 2, GL_FLOAT, GL_FALSE, 0, nullptr);
 
 	m_nbo = std::make_unique<GL::Buffer>(ARRAY_BUFFER);
 	m_nbo->Bind();
 	m_nbo->Update(static_cast<unsigned int>(m_normals.size() * sizeof(glm::vec3)), m_normals.data());
 
 	glEnableVertexAttribArray(2);
-	glVertexAttribPointer(2, 3, GL_FLOAT, GL_FALSE, 0, (void*)0);
+	glVertexAttribPointer(2, 3, GL_FLOAT, GL_FALSE, 0, nullptr);
 
 	m_mbo = std::make_unique<GL::Buffer>(ARRAY_BUFFER);
 	m_mbo->Bind();
 	m_mbo->Update(static_cast<unsigned int>(m_materials.size() * sizeof(std::uint32_t)), m_materials.data());
 
 	glEnableVertexAttribArray(3);
-	glVertexAttribPointer(3, 3, GL_FLOAT, GL_FALSE, 0, (void*)0);
+	glVertexAttribPointer(3, 3, GL_FLOAT, GL_FALSE, 0, nullptr);
 
 	m_fbo = std::make_unique<GL::Buffer>(ELEMENT_ARRAY_BUFFER);
 	m_fbo->Bind();
@@ -112,7 +112,7 @@ void GL::Terrain::Render(IShader& shader)
 	//used for tesselation
 	glPatchParameteri(GL_PATCH_VERTICES, 3);
 	m_fbo->Bind();
-	glDrawElements(GL_PATCHES, (GLsizei)m_faces.size(), GL_UNSIGNED_INT, (void*)0);
+	glDrawElements(GL_PATCHES, (GLsizei)m_faces.size(), GL_UNSIGNED_INT, nullptr);
 }
 
 void GL::Terrain::Update()
