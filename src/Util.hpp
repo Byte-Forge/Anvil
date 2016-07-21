eadc4f1614ba47e51cc2f799a41701901e4abbcb
Tarcontar <michael-schnabel@gmx.net>
2016-07-21 15:44:55 +0200

    little comment in util.hpp

--- src/Util.hpp
+++ src/Util.hpp
@@ -143,6 +143,7 @@ namespace anvil
 	 */
 	inline std::vector<std::string> &split(const std::string &s, char delim, std::vector<std::string> &elems)
 	{
+		///sure this function should return elems???
 		std::stringstream ss(s);
 		std::string item;
 		while (std::getline(ss, item, delim))
