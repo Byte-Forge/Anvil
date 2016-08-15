dc9f83f94c143571c732f4d2bf8b0f4f224358d0
Stephan Vedder <stephan.vedder@gmail.com>
2016-08-15 10:58:30 +0200

    Log opengl errors in console

--- src/Graphics/GL/RendererGL.cpp
+++ src/Graphics/GL/RendererGL.cpp
@@ -43,48 +43,27 @@ std::map<std::string, IRenderer::Vendor> vendorMap =
 
 #if 1
 void APIENTRY debugCallback(GLenum source, GLenum type, GLuint id,
-                               GLenum severity, GLsizei length, const GLchar* message, const void* userParam)
+                           GLenum severity, GLsizei length, const GLchar* message, const void* userParam)
 {
 	
-	if (type == GL_DEBUG_TYPE_OTHER_ARB)
-		return;
-	
-    std::cout << "--GL DEBUG--" << std::endl;
-    std::cout << "Message: "<< message << std::endl;
-    std::cout << "Type: ";
-    switch (type) {
+	Logger::Type loglevel = Logger::LOG_INFO;
+    
+    switch (type) 
+    {
         case GL_DEBUG_TYPE_ERROR_ARB:
-			std::cout << "ERROR";
+			loglevel = Logger::LOG_INFO;
             break;
         case GL_DEBUG_TYPE_DEPRECATED_BEHAVIOR_ARB:
-            std::cout << "DEPRECATED_BEHAVIOR";
             break;
         case GL_DEBUG_TYPE_UNDEFINED_BEHAVIOR_ARB:
-            std::cout << "UNDEFINED_BEHAVIOR";
+            loglevel = Logger::LOG_WARNING;
             break;
         case GL_DEBUG_TYPE_PORTABILITY_ARB:
-            std::cout << "PORTABILITY";
             break;
         case GL_DEBUG_TYPE_PERFORMANCE_ARB:
-            std::cout << "PERFORMANCE";
             break;
     }
-    std::cout << std::endl;
-
-    std::cout << "ID: " << id << std::endl;
-    std::cout << "SEVERITY: ";
-    switch (severity){
-        case GL_DEBUG_SEVERITY_LOW_ARB:
-            std::cout << "LOW";
-            break;
-        case GL_DEBUG_SEVERITY_MEDIUM_ARB:
-            std::cout << "MEDIUM";
-            break;
-        case GL_DEBUG_SEVERITY_HIGH_ARB:
-            std::cout << "HIGH";
-            break;
-    }
-    std::cout << std::endl;
+   	Logger::Print("OpenGL: "+std::string(message),loglevel);
 
 	if (type == GL_DEBUG_TYPE_ERROR_ARB)
 	{
